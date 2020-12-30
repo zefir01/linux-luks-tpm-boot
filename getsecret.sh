e531ae0dd74a7c2812ca962576c69c2c7efaac3a
zefir01 <petr.marketlab@gmail.com>
2020-12-30 23:30:22 +0300

    bug

--- getsecret.sh
+++ getsecret.sh
@@ -8,13 +8,13 @@ if tpm_nvread -i $INDEX -f /tmp/key >/dev/null; then
     rm /tmp/key
     # read the content again with size 0, to disable reading until reboot
     # For some reason, tcsd stops after the first command, so we have to start it again first and wait 1s for it to be ready
-    tcsd && sleep 1
-    tpm_nvread -i $INDEX -s 0 >> /dev/null
+    #tcsd && sleep 1
+    #tpm_nvread -i $INDEX -s 0 >> /dev/null
 else
     IN_FD="/proc/self/fd/2"
     echo >&2 "TPM failed, please enter fallback passphrase:"
     stty <$IN_FD -echo
-    read <$IN_FD -rs -t 10 key
+    read <$IN_FD -rs key
     stty <$IN_FD echo
     echo -n $key
 fi
